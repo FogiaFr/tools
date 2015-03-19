3a78124d399fe587b3412d5dd5e254f97ce49e70
unknown <MC07461S@DSP0698464.hercule.edf.fr>
2015-03-19 16:49:46 +0100

    Gestion des enclaves

--- src/main/java/com/mkl/tools/eu/MapGenerator.java
+++ src/main/java/com/mkl/tools/eu/MapGenerator.java
@@ -1,6 +1,7 @@
 package com.mkl.tools.eu;
 
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +28,7 @@ import java.util.regex.Pattern;
 public class MapGenerator {
     /**
      * Do all the stuff.
+     *
      * @param args no args.
      * @throws Exception exception.
      */
@@ -33,13 +36,10 @@ public class MapGenerator {
         String ligne;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(MapGenerator.class.getClassLoader().getResourceAsStream("chemins.eps")));
-        Map<String, List<Pair<Integer, Integer>>> paths = new HashMap<>();
-        List<Pair<Integer, Integer>> currentPath = null;
-        Map<String, List<List<Pair<Integer, Integer>>>> provinces = new HashMap<>();
-        List<List<Pair<Integer, Integer>>> currentProv = null;
-        String currentName = null;
-        boolean isParsingPath = false;
-        boolean isParsingProvince = false;
+        Map<String, Path> paths = new HashMap<>();
+        Path currentPath = null;
+        Map<String, Province> provinces = new HashMap<>();
+        Province currentProv = null;
         Pattern mer = Pattern.compile("/mer\\d+ beginpath.*");
         Pattern bord = Pattern.compile("/bord\\d+ beginpath.*");
         Pattern path = Pattern.compile("/path\\d+ beginpath.*");
@@ -47,93 +47,99 @@ public class MapGenerator {
         while ((ligne = reader.readLine()) != null) {
             Matcher provMatch = province.matcher(ligne);
             if (mer.matcher(ligne).matches() || bord.matcher(ligne).matches() || path.matcher(ligne).matches()) {
-                isParsingPath = true;
-                currentPath = new ArrayList<>();
-                currentName = ligne.split(" ")[0];
-            } else if (ligne.startsWith("endpath") && isParsingPath) {
-                paths.put(currentName, currentPath);
+                currentPath = new Path(ligne.split(" ")[0], !ligne.contains("contpath"));
+            } else if (ligne.startsWith("endpath") && currentPath != null) {
+                paths.put(currentPath.getName(), currentPath);
                 currentPath = null;
-                currentName = null;
-                isParsingPath = false;
-            } else if (isParsingPath) {
+            } else if (currentPath != null) {
                 String[] coords = ligne.split(" ");
                 for (int i = 0; i < ligne.length() - 1; i = i + 2) {
                     try {
                         Integer x = Integer.parseInt(coords[i]);
                         Integer y = Integer.parseInt(coords[i + 1]);
-                        currentPath.add(new ImmutablePair<>(x, y));
+                        currentPath.getCoords().add(new ImmutablePair<>(x, y));
                     } catch (NumberFormatException e) {
                         break;
                     }
                 }
             } else if (provMatch.matches()) {
-                if (isParsingProvince) {
-                    provinces.put(currentName, currentProv);
+                if (currentProv != null) {
+                    provinces.put(currentProv.getName(), currentProv);
                 }
-                isParsingProvince = true;
-                currentName = provMatch.group(1);
-                currentProv = new ArrayList<>();
-            } else if (isParsingProvince) {
+                String fullName = provMatch.group(1);
+                Matcher m = Pattern.compile("(.*) <(.*)>").matcher(fullName);
+                if (m.matches()) {
+                    currentProv = new Province(m.group(1), m.group(2));
+                } else {
+                    currentProv = new Province(fullName, null);
+                }
+            } else if (currentProv != null) {
                 if (ligne.startsWith("/prov")) {
-                    List<Pair<Integer, Integer>> provPiece = new ArrayList<>();
-                    Matcher m = Pattern.compile("(/path\\d+)|(/bord\\d+)|(/mer\\d+)").matcher(ligne);
+                    currentProv.setTerrain(ligne.split(" ")[1]);
+                    Matcher m = Pattern.compile("(/path\\d+ AR?)|(/bord\\d+ AR?)|(/mer\\d+ AR?)").matcher(ligne);
                     while (m.find()) {
-                        String pathFound = m.group();
-                        List<Pair<Integer, Integer>> coordsFound = paths.get(pathFound);
-                        if (coordsFound != null) {
-                            provPiece.addAll(coordsFound);
+                        String chaine = m.group();
+                        Path pathFound = paths.get(chaine.split(" ")[0]);
+                        if (pathFound != null) {
+                            currentProv.getPaths().add(new DirectedPath(pathFound, chaine.split(" ")[1].contains("R")));
                         } else {
-                            System.out.println("Oops, le chemin " + pathFound + " n'a pas ete trouve pour " + currentName);
+                            System.out.println("Oops, le chemin " + pathFound + " n'a pas ete trouve pour " + currentProv.getName());
                         }
                     }
-                    currentProv.add(provPiece);
                 }
             }
             // Bad parsing of provinces, does not detect some cases.
         }
 
-        if (isParsingProvince) {
-            provinces.put(currentName, currentProv);
+        if (currentProv != null) {
+            provinces.put(currentProv.getName(), currentProv);
         }
 
         reader.close();
+
+        extractData(provinces);
+    }
+
+    private static void extractData(Map<String, Province> provinces) throws Exception {
+
 
         Writer writer = createFileWriter("src/main/resources/countries.geo.json", false);
         writer.append("{\"type\":\"FeatureCollection\",\"features\":[\n");
         boolean first = true;
 
-        for (String prov: provinces.keySet()) {
-            if (! first) {
+        for (String prov : provinces.keySet()) {
+            if (!first) {
                 writer.append(",\n");
             } else {
                 first = false;
             }
             writer.append("    {\"type\":\"Feature\",\"geometry\":{\"type\":\"");
-            List<List<Pair<Integer, Integer>>> polygones = provinces.get(prov);
-            if (polygones.size() == 1) {
+            Province polygones = provinces.get(prov);
+            polygones.restructurate();
+//            if (polygones.getCoords().size() == 1) {
                 writer.append("Polygon");
-            } else if (polygones.size() > 1) {
-                writer.append("MultiPolygon");
-            } else {
-                System.out.println("Oops, la province " + prov + " n'a pas de frontieres.");
-            }
+//            } else if (polygones.getCoords().size() > 1) {
+//                writer.append("MultiPolygon");
+//            } else {
+//                System.out.println("Oops, la province " + polygones.getName() + " n'a pas de frontieres.");
+//            }
             writer.append("\",\"coordinates\":[");
 
             boolean firstPolygon = true;
-            for (List<Pair<Integer, Integer>> polygone: polygones) {
-                if (! firstPolygon) {
+            for (List<Pair<Integer, Integer>> polygone : polygones.getCoords()) {
+                if (!firstPolygon) {
                     writer.append(", ");
                 } else {
                     firstPolygon = false;
                 }
                 writer.append("[");
-                if (polygones.size() > 1) {
-                    writer.append("[");
-                }
+//                if (polygones.getCoords().size() > 1) {
+//                    writer.append("[");
+//                }
 
                 boolean firstCoord = true;
-                for (Pair<Integer, Integer> coord: polygone) {
-                    if (! firstCoord) {
+                for (Pair<Integer, Integer> coord : polygone) {
+                    if (!firstCoord) {
                         writer.append(", ");
                     } else {
                         firstCoord = false;
@@ -143,14 +149,14 @@ public class MapGenerator {
                     writer.append("[").append(Double.toString(x)).append(", ").append(Double.toString(y)).append("]");
                 }
 
-                if (polygones.size() > 1) {
-                    writer.append("]");
-                }
+//                if (polygones.getCoords().size() > 1) {
+//                    writer.append("]");
+//                }
 
                 writer.append("]");
             }
 
-            writer.append("]},\"id\":\"").append(prov).append("\"}");
+            writer.append("]},\"id\":\"").append(polygones.getName()).append("\"}");
         }
 
         writer.append("\n]}");
@@ -188,5 +194,290 @@ public class MapGenerator {
         }
 
         return writer;
+    }
+
+    /**
+     * Returns the first element of the list.
+     *
+     * @param list the list.
+     * @param <E>  generic of the list.
+     * @return the first element of the list.
+     */
+    private static <E> E firstElement(List<E> list) {
+        E elem = null;
+
+        if (list != null && !list.isEmpty()) {
+            elem = list.get(0);
+        }
+
+        return elem;
+    }
+
+    /**
+     * Returns the last element of the list.
+     *
+     * @param list the list.
+     * @param <E>  generic of the list.
+     * @return the last element of the list.
+     */
+    private static <E> E lastElement(List<E> list) {
+        E elem = null;
+
+        if (list != null && !list.isEmpty()) {
+            elem = list.get(list.size() - 1);
+        }
+
+        return elem;
+    }
+
+    /**
+     * Return the distance that is needed in order to close the polygone. Returns 0 if the polygone is closed.
+     *
+     * @param polygone to check.
+     * @return the distance that is needed in order to close the polygone. Returns 0 if the polygone is closed.
+     */
+    private static double distanceToClosePolygone(List<Pair<Integer, Integer>> polygone) {
+        return distance(firstElement(polygone), lastElement(polygone));
+    }
+
+    /**
+     * Returns the distance between the two points.
+     *
+     * @param first  one point.
+     * @param second another point.
+     * @return the distance between the two points.
+     */
+    private static double distance(Pair<Integer, Integer> first, Pair<Integer, Integer> second) {
+        if (first == null || second == null) {
+            return 0;
+        }
+
+        return Math.sqrt((first.getLeft() - second.getLeft()) * (first.getLeft() - second.getLeft()) + (first.getRight() - second.getRight()) * (first.getRight() - second.getRight()));
+    }
+
+    /** Inner class describing a path (can be border, mer or path). */
+    private static class Path {
+        /** Name of the path. */
+        private String name;
+        /** Flag saying that the path begins by itself (and does not continue the previous one). */
+        private boolean begin;
+        /** Coordinates of the path. */
+        private List<Pair<Integer, Integer>> coords = new ArrayList<>();
+
+        /**
+         * Constructor.
+         *
+         * @param name  of the path.
+         * @param begin of the path.
+         */
+        public Path(String name, boolean begin) {
+            this.name = name;
+            this.begin = begin;
+        }
+
+        /** @return the name. */
+        public String getName() {
+            return name;
+        }
+
+        /** @return the begin. */
+        public boolean isBegin() {
+            return begin;
+        }
+
+        /** @return the coords. */
+        public List<Pair<Integer, Integer>> getCoords() {
+            return coords;
+        }
+
+        /**
+         * Returns the inverted coords of the path.
+         *
+         * @return the inverted coords of the path.
+         */
+        public List<Pair<Integer, Integer>> getInvertedCoords() {
+            List<Pair<Integer, Integer>> invertedCoords = new ArrayList<>(coords);
+            Collections.reverse(invertedCoords);
+
+            return invertedCoords;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+
+            boolean equals = false;
+
+            if (obj instanceof Path) {
+                equals = StringUtils.equals(name, ((Path) obj).getName());
+            }
+
+            return equals;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+    }
+
+    /** Inner class describing a directed path (inversed or not). */
+    private static class DirectedPath {
+        /** the path. */
+        private Path path;
+        /** the flag saying that the path is inversed or not. */
+        private boolean inverse;
+
+        /**
+         * Constructor.
+         *
+         * @param path    the path.
+         * @param inverse the inverse.
+         */
+        public DirectedPath(Path path, boolean inverse) {
+            this.path = path;
+            this.inverse = inverse;
+        }
+
+        /** @return the path. */
+        public Path getPath() {
+            return path;
+        }
+
+        /** @return the inverse. */
+        public boolean isInverse() {
+            return inverse;
+        }
+    }
+
+    /** Inner class describing a province. */
+    private static class Province {
+        /** The name of the province. */
+        private String name;
+        /** The religion of the province. */
+        private String religion;
+        /** The terrain of the province. */
+        private String terrain;
+        /** The paths representing the frontiers of the province. */
+        private List<DirectedPath> paths = new ArrayList<>();
+        /** Restructuration of the coords for the geo.json export. */
+        private List<List<Pair<Integer, Integer>>> coords;
+
+        /**
+         * Constructor.
+         *
+         * @param name     of the province.
+         * @param religion of the province.
+         */
+        public Province(String name, String religion) {
+            this.name = name;
+            this.religion = religion;
+        }
+
+        /** @return the name. */
+        public String getName() {
+            return name;
+        }
+
+        /** @return the religion. */
+        public String getReligion() {
+            return religion;
+        }
+
+        /** @return the terrain. */
+        public String getTerrain() {
+            return terrain;
+        }
+
+        /** @param terrain the terrain to set. */
+        public void setTerrain(String terrain) {
+            this.terrain = terrain;
+        }
+
+        /** @return the paths. */
+        public List<DirectedPath> getPaths() {
+            return paths;
+        }
+
+        /** @return the coords. */
+        public List<List<Pair<Integer, Integer>>> getCoords() {
+            return coords;
+        }
+
+        /** Generates the restructurated coords of the province. */
+        public void restructurate() {
+            if (getName().equals("Norvege")) {
+                int a = 1;
+            }
+            coords = new ArrayList<>();
+            coords.add(new ArrayList<>());
+
+            for (DirectedPath path : getPaths()) {
+                List<Pair<Integer, Integer>> pathValues;
+                if (path.isInverse()) {
+                    pathValues = path.getPath().getInvertedCoords();
+                } else {
+                    pathValues = path.getPath().getCoords();
+                }
+
+                if (path.getPath().isBegin() && !lastElement(coords).isEmpty() && !pathValues.isEmpty()) {
+                    double nextDistance = distance(lastElement(lastElement(coords)), firstElement(pathValues));
+                    if (nextDistance > 0) {
+                        double distance = distanceToClosePolygone(lastElement(coords));
+
+                        if (distance > 0) {
+                            if (distance > nextDistance) {
+                                System.out.println("OK " + getName() + " n'est pas fermé à " + path.getPath().getName() + " ! " + firstElement(lastElement(coords)) + " vs " + lastElement(lastElement(coords)));
+                            } else {
+                                System.out.println( "KO " + getName() + " n'est pas fermé à " + path.getPath().getName() + " ! " + firstElement(lastElement(coords)) + " vs " + lastElement(lastElement(coords)));
+                                coords.add(new ArrayList<>());
+                            }
+                        } else {
+                            coords.add(new ArrayList<>());
+                        }
+                    }
+                }
+
+                lastElement(coords).addAll(pathValues);
+            }
+
+            double distance = distanceToClosePolygone(lastElement(coords));
+
+
+            if (distance > 0) {
+                System.out.println(getName() + " n'est pas fermé à " + firstElement(getPaths()).getPath().getName()
+                        + " ! " + firstElement(lastElement(coords)) + " vs " + lastElement(lastElement(coords)));
+            }
+
+//            if (getPaths().get(0).getPath().isBegin() && ! coords.isEmpty() && ! coords.get(coords.size() - 1).equals(coords.get(0))) {
+//                System.out.println(getName() + " a une discontinuite en " + getPaths().get(0).getPath().getName() + " : " + coords.get(coords.size() - 1) + " vs " + coords.get(0));
+//            }
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+
+            boolean equals = false;
+
+            if (obj instanceof Province) {
+                equals = StringUtils.equals(name, ((Province) obj).getName());
+            }
+
+            return equals;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
     }
 }
